610f99478ba535c16f40d2dee117b9f5f92fe0e9
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-08-02 03:40:14 +0000

    :construction: Type alias for images.

---
+++ cpp/include/func_sketch/plotter/image.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2026 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of Image type.
+ */
+#pragma once
+
+#include <opencv2/core.hpp>
+
+namespace func_sketch::plotter {
+
+/*!
+ * \brief Image type.
+ */
+using Image = cv::Mat;
+
+}  // namespace func_sketch::plotter
