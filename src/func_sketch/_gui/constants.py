c079a700b0c1116861c1edf7546ec33f03bbb270
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-08-06 14:39:49 +0000

    :construction: Small tuning of GUI.

--- src/func_sketch/_gui/constants.py
+++ src/func_sketch/_gui/constants.py
@@ -1,7 +1,28 @@
 """Constants for the GUI."""
 
+# Colors
+
 TEXT_INPUT_NORMAL_BACKGROUND_COLOR = (1.0, 1.0, 1.0, 1.0)
 """Background color for normal text input fields."""
 
 TEST_INPUT_ERROR_BACKGROUND_COLOR = (1.0, 0.8, 0.8, 1.0)
 """Background color for text input fields with invalid input."""
+
+# Font sizes
+
+FONT_SIZE_DEFAULT = 15
+"""Default font size in Kivy."""
+
+FONT_SIZE_HEADER1 = 25
+"""Font size for level 1 headers."""
+
+FONT_SIZE_HEADER2 = 20
+"""Font size for level 2 headers."""
+
+FONT_SIZE_TEXT = 15
+"""Font size for normal text."""
+
+# Spacing
+
+SPACING_DEFAULT = 10
+"""Default spacing between widgets."""
