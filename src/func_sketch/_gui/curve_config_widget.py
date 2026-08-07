3b62e420267c5ee1a6b0f6f3fac0d2ab71fa0a22
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-08-07 15:23:01 +0000

    :recycle: Change the widget name.

--- src/func_sketch/_gui/curve_config_widget.py
+++ src/func_sketch/_gui/curve_config_widget.py
@@ -1,11 +1,11 @@
-"""Class of widgets to configure curves defined by explicit functions."""
+"""Class of widgets to configure curves."""
 
 import kivy.properties
 import kivy.uix.boxlayout
 
 
-class ExplicitCurveConfigWidget(kivy.uix.boxlayout.BoxLayout):
-    """Class of widgets to configure curves defined by explicit functions."""
+class CurveConfigWidget(kivy.uix.boxlayout.BoxLayout):
+    """Class of widgets to configure curves."""
 
     curve_name = kivy.properties.StringProperty("Function")
     error_message = kivy.properties.StringProperty("")
