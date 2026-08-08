47305287a424f39f6583dd378e8d48a4b7bca4d5
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-08-08 07:51:19 +0000

    :wrench: Fix coverage measurement.

--- scripts/collect_llvm_coverage.sh
+++ scripts/collect_llvm_coverage.sh
@@ -12,15 +12,18 @@ ls -lh $DIR/coverage/
 
 # make a file for codecov
 llvm-profdata merge -o $DIR/coverage/coverage.profdata $DIR/coverage/coverage_*.profraw
-opts="-ignore-filename-regex='.*/(tests|.venv|.pyenv)/.*' -instr-profile=$DIR/coverage/coverage.profdata"
+opts=(
+    "-ignore-filename-regex=.*/(tests|.venv|.pyenv)/.*"
+    "-instr-profile=$DIR/coverage/coverage.profdata"
+)
 for a in $(find $DIR/bin/func_sketch_test_units $ROOT_DIR/src/func_sketch/_cpp.*.so); do
-    opts="$opts -object $a"
+    opts+=("-object" "$a")
 done
-llvm-cov show $opts >$DIR/coverage/coverage.txt
-llvm-cov show $opts -format=html -output-dir=$DIR/coverage/html
+llvm-cov show "${opts[@]}" >$DIR/coverage/coverage.txt
+llvm-cov show "${opts[@]}" -format=html -output-dir=$DIR/coverage/html
 
 # output in lcov's format
-llvm-cov export $opts -format=lcov > $DIR/coverage/coverage
+llvm-cov export "${opts[@]}" -format=lcov > $DIR/coverage/coverage
 
 # output summary to stdout
-llvm-cov report $opts | tee $DIR/coverage/coverage_summary.txt
+llvm-cov report "${opts[@]}" | tee $DIR/coverage/coverage_summary.txt
