d2a5dd1a17adfda9e055e86958b08cdcccc44f6d
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-09-06 02:46:01 +0000

    :wrench: CI configuration for MSVC.

---
+++ scripts/ci_win_test.cmd
@@ -0,0 +1,20 @@
+@REM Below is used in my runner.
+call "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat" x86_x64
+
+cmake .. ^
+    -G Ninja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DFUNC_SKETCH_BUILD_TESTS:BOOL=ON ^
+    -DFUNC_SKETCH_ENABLE_CCACHE:BOOL=ON ^
+    -DFUNC_SKETCH_ENABLE_AUSAN:BOOL=OFF ^
+    -DFUNC_SKETCH_WRITE_JUNIT:BOOL=ON
+
+cmake --build . --config Release --parallel
+
+ctest -V --build-config Release
+
+cd ..
+
+poetry run pytest tests -v
+poetry run pytest bench -v --benchmark-json=benchmark.json
+poetry run python scripts/generate_benchmark_metrics.py benchmark.json bench_metrics.txt
