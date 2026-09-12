114fc03fc7bf2d4569f4d3c4caff929ca978eed8
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-09-12 13:23:51 +0000

    :recycle: Simpler implementation of operators.

--- cpp/include/func_sketch/plotter/rgb_color.h
+++ cpp/include/func_sketch/plotter/rgb_color.h
@@ -37,31 +37,15 @@ struct RGBColor {
 
     //! Blue component.
     std::uint8_t b;
+
+    /*!
+     * \brief Check if this color is equal to another color.
+     *
+     * \retval true Colors are equal.
+     * \retval false Colors are not equal.
+     */
+    [[nodiscard]] bool operator==(const RGBColor& /*other*/) const = default;
 };
-
-/*!
- * \brief Check if two RGBColor objects are equal.
- *
- * \param[in] left Left object.
- * \param[in] right Right object.
- * \retval true Two objects are equal.
- * \retval false Two objects are not equal.
- */
-inline bool operator==(const RGBColor& left, const RGBColor& right) {
-    return left.r == right.r && left.g == right.g && left.b == right.b;
-}
-
-/*!
- * \brief Check if two RGBColor objects are equal.
- *
- * \param[in] left Left object.
- * \param[in] right Right object.
- * \retval true Two objects are not equal.
- * \retval false Two objects are equal.
- */
-inline bool operator!=(const RGBColor& left, const RGBColor& right) {
-    return !(left == right);
-}
 
 }  // namespace func_sketch::plotter
 
