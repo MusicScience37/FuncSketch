52a3587d4f4c20194634cf8938f2da48beb40d9d
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-09-20 13:59:36 +0000

    :recycle: Change directory structure of tests.

--- tests/gui/common/__init__.py
+++ tests/gui/common/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests of the internal logic."""
+"""Tests of the common GUI components."""
