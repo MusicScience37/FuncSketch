eca70044c929be83dbacd4bac3ea1b9700ebfcbc
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-09-21 09:59:22 +0000

    :recycle: Separate common part of the menu bar.

--- src/func_sketch/_gui/plot_2d/menu_widget.py
+++ src/func_sketch/_gui/plot_2d/menu_widget.py
@@ -17,150 +17,16 @@
 import logging
 import tkinter
 import tkinter.filedialog
-import typing
 
-import kivy.graphics
-import kivy.uix.behaviors.button
-import kivy.uix.boxlayout
-import kivy.uix.dropdown
-import kivy.uix.label
+import kivy.properties
 
 from func_sketch._cpp import save_image
-from func_sketch._gui.common.constants import (
-    MENU_BAR_BACKGROUND_COLOR,
-    MENU_DROP_DOWN_WIDTH,
-    PADDING_MENU_BAR,
-    PADDING_MENU_BUTTON,
-    SPACING_MENU_BAR,
-)
+from func_sketch._gui.common.menu_bar import MenuBar
 
 LOGGER = logging.getLogger(__name__)
 
 
-class CommandButton(kivy.uix.behaviors.button.ButtonBehavior, kivy.uix.label.Label):
-    """Class of the command button in the menu."""
-
-    def __init__(self, callback: typing.Callable[[], None], **kwargs) -> None:
-        """Constructor.
-
-        Args:
-            callback: Callback function to be called when the button is pressed.
-            **kwargs: Additional keyword arguments for the Label.
-        """
-        super().__init__(**kwargs)
-
-        self.size_hint_y = None
-        self.height = self.texture_size[1]
-        self.bind(
-            texture_size=lambda _instance, value: setattr(self, "height", value[1])
-        )
-
-        self.text_size = (self.width, None)
-        self.bind(
-            width=lambda _instance, value: setattr(self, "text_size", (value, None))
-        )
-
-        self.halign = "left"
-        self.valign = "middle"
-
-        self.padding = PADDING_MENU_BUTTON + SPACING_MENU_BAR
-
-        self.bind(on_release=lambda instance: callback())
-
-
-class MenuDropDown(kivy.uix.dropdown.DropDown):
-    """Class of the dropdown menu in the menu bar."""
-
-    def __init__(
-        self, items: list[tuple[str, typing.Callable[[], None]]], **kwargs
-    ) -> None:
-        """Constructor.
-
-        Args:
-            items: List of tuples containing the menu item text and the corresponding callback function.
-            **kwargs: Additional keyword arguments for the DropDown.
-        """
-        super().__init__(**kwargs)
-
-        self._items = items
-
-        with self.canvas.before:
-            kivy.graphics.Color(rgba=MENU_BAR_BACKGROUND_COLOR)
-            self._background_rect = kivy.graphics.Rectangle(
-                pos=self.pos, size=self.size
-            )
-        self.bind(
-            pos=lambda _instance, value: setattr(self._background_rect, "pos", value)
-        )
-        self.bind(
-            size=lambda _instance, value: setattr(self._background_rect, "size", value)
-        )
-
-        self.auto_width = False
-
-        self._buttons: list[CommandButton] = []
-        for item_text, _ in items:
-            button = CommandButton(
-                text=item_text,
-                callback=lambda item_text=item_text: self.select(item_text),  # type: ignore
-            )
-            button.bind(texture_size=lambda _instance, _value: self._update_width())
-            self.add_widget(button)
-            self._buttons.append(button)
-
-        self._update_width()
-
-    def _update_width(self) -> None:
-        """Update the width."""
-        self.width = max(
-            max(
-                (child.texture_size[0] for child in self._buttons),
-                default=MENU_DROP_DOWN_WIDTH,
-            ),
-            MENU_DROP_DOWN_WIDTH,
-        )
-
-    def on_select(self, item_text: str) -> None:
-        """Handle the selection of a menu item."""
-        for text, callback in self._items:
-            if text == item_text:
-                callback()
-                break
-
-
-class MenuButton(kivy.uix.behaviors.button.ButtonBehavior, kivy.uix.label.Label):
-    """Class of the button in the menu bar."""
-
-    def __init__(
-        self, items: list[tuple[str, typing.Callable[[], None]]], **kwargs
-    ) -> None:
-        """Constructor.
-
-        Args:
-            items: List of tuples containing the button text and the corresponding callback function.
-            **kwargs: Additional keyword arguments for the Label.
-        """
-        super().__init__(**kwargs)
-
-        self.size_hint_x = None
-        self.width = self.texture_size[0]
-        self.bind(
-            texture_size=lambda _instance, value: setattr(self, "width", value[0])
-        )
-
-        self.size_hint_y = None
-        self.height = self.texture_size[1]
-        self.bind(
-            texture_size=lambda _instance, value: setattr(self, "height", value[1])
-        )
-
-        self.padding = PADDING_MENU_BUTTON
-
-        self.dropdown = MenuDropDown(items=items)
-        self.bind(on_release=self.dropdown.open)
-
-
-class MenuWidget(kivy.uix.boxlayout.BoxLayout):
+class MenuWidget(MenuBar):
     """Class of the widget of the menu bar."""
 
     shared_state = kivy.properties.ObjectProperty()
@@ -172,36 +38,17 @@ class MenuWidget(kivy.uix.boxlayout.BoxLayout):
         Args:
             **kwargs: Additional keyword arguments for the BoxLayout.
         """
-        super().__init__(**kwargs)
-
-        self.canvas.before.clear()
-        with self.canvas.before:
-            kivy.graphics.Color(rgba=MENU_BAR_BACKGROUND_COLOR)
-            self._background_rect = kivy.graphics.Rectangle(
-                pos=self.pos, size=self.size
-            )
-        self.bind(
-            pos=lambda _instance, value: setattr(self._background_rect, "pos", value)
+        super().__init__(
+            menu_contents=[
+                (
+                    "File",
+                    [
+                        ("Save plot", self._save_plot),
+                    ],
+                ),
+            ],
+            **kwargs,
         )
-        self.bind(
-            size=lambda _instance, value: setattr(self._background_rect, "size", value)
-        )
-
-        self.add_widget(
-            MenuButton(
-                text="File",
-                items=[
-                    ("Save plot", self._save_plot),
-                ],
-            )
-        )
-
-        self.size_hint_y = None
-        self.height = self.minimum_height
-        self.bind(minimum_height=self.setter("height"))
-
-        self.padding = PADDING_MENU_BAR
-        self.spacing = SPACING_MENU_BAR
 
     def _save_plot(self) -> None:
         """Save the current plot."""
