f62ea4ac6c01678ee4a7191b9c432e2206f4d249
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-09-26 08:39:40 +0000

    :wrench: Set the size of screen in xvfb.

--- scripts/run_pytest.sh
+++ scripts/run_pytest.sh
@@ -25,7 +25,7 @@
 cd $(dirname $0)/..
 
 if [ "$#" -ge 1 ]; then
-    xvfb-run poetry run pytest "$@"
+    xvfb-run --server-args="-screen 0 1300x800x24" poetry run pytest "$@"
 else
-    xvfb-run poetry run pytest tests
+    xvfb-run --server-args="-screen 0 1300x800x24" poetry run pytest tests
 fi
