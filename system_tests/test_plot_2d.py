0875c0b6a3d90ef98c1380e50b3452001effe776
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-09-26 13:00:51 +0000

    :white_check_mark: Test to fix image size in system tests.

--- system_tests/test_plot_2d.py
+++ system_tests/test_plot_2d.py
@@ -17,7 +17,9 @@
 import kivy.core.window
 import pytest
 
+from func_sketch._gui.common.collapsible_box import CollapsibleBox
 from func_sketch._gui.common.constants import NUM_CURVES
+from func_sketch._gui.common.plain_text_input import PlainTextInput
 from func_sketch._gui.common.switch_widget import SwitchWidget
 from func_sketch._gui.plot_2d.curve_spec_list_widget import CurveSpecListWidget
 from func_sketch._gui.plot_2d.curve_spec_widget import CurveSpecWidget
@@ -26,6 +28,8 @@ from func_sketch._gui.plot_2d.expression_text_input_model import (
     ExpressionTextInputModel,
 )
 from func_sketch._gui.plot_2d.func_sketch_app import FuncSketchApp
+from func_sketch._gui.plot_2d.image_size_widget import ImageSizeWidget
+from func_sketch._gui.plot_2d.shared_state import SharedState
 from system_tests.screen_saver import ScreenshotSaver
 from system_tests.util import wait_window_change
 
@@ -167,3 +171,69 @@ def test_curves(
     curve_spec_3_widget.ids.expression_text_input.text = "log10(x)"
     wait_window_change()
     screenshot_saver.save("curve3_set")
+
+
+def test_fix_image_size(
+    func_sketch_plot_2d_app: FuncSketchApp, screenshot_saver: ScreenshotSaver
+) -> None:
+    """Test to fix the image size."""
+    screenshot_saver.save("initial")
+
+    curves_collapsible_box = func_sketch_plot_2d_app.root.ids.curves_collapsible_box
+    assert isinstance(curves_collapsible_box, CollapsibleBox)
+    range_collapsible_box = func_sketch_plot_2d_app.root.ids.range_collapsible_box
+    assert isinstance(range_collapsible_box, CollapsibleBox)
+    image_size_collapsible_box = (
+        func_sketch_plot_2d_app.root.ids.image_size_collapsible_box
+    )
+    assert isinstance(image_size_collapsible_box, CollapsibleBox)
+    titles_collapsible_box = func_sketch_plot_2d_app.root.ids.titles_collapsible_box
+    assert isinstance(titles_collapsible_box, CollapsibleBox)
+
+    curves_collapsible_box.collapsed = True
+    range_collapsible_box.collapsed = True
+    image_size_collapsible_box.collapsed = False
+    titles_collapsible_box.collapsed = True
+    wait_window_change()
+    screenshot_saver.save("show_image_size_section")
+
+    image_size_widget = func_sketch_plot_2d_app.root.ids.image_size_widget
+    assert isinstance(image_size_widget, ImageSizeWidget)
+    fix_size_switch = image_size_widget.ids.fix_size_switch
+    assert isinstance(fix_size_switch, SwitchWidget)
+    fixed_width_input = image_size_widget.ids.fixed_width_input
+    assert isinstance(fixed_width_input, PlainTextInput)
+    fixed_height_input = image_size_widget.ids.fixed_height_input
+    assert isinstance(fixed_height_input, PlainTextInput)
+    shared_state = func_sketch_plot_2d_app.root.shared_state
+    assert isinstance(shared_state, SharedState)
+
+    image_buffer = shared_state.image_buffer
+    assert image_buffer is not None
+    assert image_buffer.shape[0] != int(fixed_height_input.text)
+    assert image_buffer.shape[1] != int(fixed_width_input.text)
+
+    fix_size_switch.active = True
+    wait_window_change()
+    screenshot_saver.save("fix_size_enabled")
+    image_buffer = shared_state.image_buffer
+    assert image_buffer is not None
+    assert image_buffer.shape[0] == int(fixed_height_input.text)
+    assert image_buffer.shape[1] == int(fixed_width_input.text)
+
+    fixed_width_input.text = "800"
+    fixed_height_input.text = "600"
+    wait_window_change()
+    screenshot_saver.save("fixed_size_changed")
+    image_buffer = shared_state.image_buffer
+    assert image_buffer is not None
+    assert image_buffer.shape[0] == int(fixed_height_input.text)
+    assert image_buffer.shape[1] == int(fixed_width_input.text)
+
+    fix_size_switch.active = False
+    wait_window_change()
+    screenshot_saver.save("fix_size_disabled")
+    image_buffer = shared_state.image_buffer
+    assert image_buffer is not None
+    assert image_buffer.shape[0] != int(fixed_height_input.text)
+    assert image_buffer.shape[1] != int(fixed_width_input.text)
